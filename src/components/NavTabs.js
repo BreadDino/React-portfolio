6b212f620a360499ca7d5861f0231e32148278de
unknown <gabemarredondo@gmail.com>
2022-11-25 16:49:24 -0800

    added correct starter code

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -11,7 +11,7 @@ function NavTabs({ currentPage, handlePageChange }) {
           onClick={() => handlePageChange('Home')}
           // This is a conditional (ternary) operator that checks to see if the current page is "Home"
           // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+          className={currentPage === 'Home' ? 'fakenav select' : 'fakenav'}
         >
           Home
         </a>
@@ -21,19 +21,9 @@ function NavTabs({ currentPage, handlePageChange }) {
           href="#about"
           onClick={() => handlePageChange('About')}
           // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+          className={currentPage === 'About' ? 'fakenav select' : 'fakenav'}
         >
           About
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#blog"
-          onClick={() => handlePageChange('Blog')}
-          // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
-        >
-          Blog
         </a>
       </li>
       <li className="nav-item">
@@ -41,7 +31,7 @@ function NavTabs({ currentPage, handlePageChange }) {
           href="#contact"
           onClick={() => handlePageChange('Contact')}
           // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+          className={currentPage === 'Contact' ? 'fakenav select' : 'fakenav'}
         >
           Contact
         </a>
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -15,9 +15,6 @@ export default function PortfolioContainer() {
     if (currentPage === 'About') {
       return <About />;
     }
-    if (currentPage === 'Blog') {
-      return <Blog />;
-    }
     return <Contact />;
   };
 
